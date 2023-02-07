02f6f8e933140ce531834a9c27292cd7a5b1ad07
MilkaII <fabiogaspar2603@gmail.com>
2023-02-07 09:29:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ var morgan = require('morgan');
       
 var app = express();
 
-const cardsRouter = require("./routes/cardsRoutes");
-const usersRouter = require("./routes/usersRoutes");
-
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const cardsRouter = require("./routes/cardsRoutes");
+const usersRouter = require("./routes/usersRoutes");
 
 app.use("/api/users",usersRouter);
 app.use("/api/cards",cardsRouter);
